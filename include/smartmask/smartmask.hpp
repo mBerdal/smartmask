bebfe8073c163e01acd760a94c30960b3884e308
Magnus Berdal <magnus.berdal@gmail.com>
2023-07-16 19:38:12 +0200

    non-templated write functionality

--- include/smartmask/smartmask.hpp
+++ include/smartmask/smartmask.hpp
@@ -9,7 +9,7 @@ template<typename Type, size_t BitPositionLeft, size_t... BitPositionsRight>
 struct Mask
 {
   static constexpr Type BITMASK{(1 << BitPositionLeft) | Mask<Type, BitPositionsRight...>::BITMASK};
-  static constexpr size_t MASKSIZE{1 + Mask<Type, BitPositionsRight...>::MASKSIZE};
+  static constexpr size_t MASKSIZE{1 + sizeof...(BitPositionsRight)};
 
   template<Type Value>
   static void writeTo(volatile Type& dest)
@@ -25,6 +25,15 @@ struct Mask
     dest = (dest & ~BITMASK) | distribute<Value>();
   }
 
+  static void writeTo(const Type value, Type& dest)
+  {
+    dest = (dest & ~BITMASK) | distribute(value);
+  }
+  static void writeTo(const Type value, volatile Type& dest)
+  {
+    dest = (dest & ~BITMASK) | distribute(value);
+  }
+
   static Type readFrom(const Type src)
   {
     return (((src & (1 << BitPositionLeft)) > 0) << (MASKSIZE - 1)) | Mask<Type, BitPositionsRight...>::readFrom(src);
@@ -35,16 +44,20 @@ struct Mask
   {
     return (((Value & (1 << (MASKSIZE - 1))) > 0) << BitPositionLeft) | Mask<Type, BitPositionsRight...>::template distribute<Value>();
   }
+  static Type distribute(const Type value)
+  {
+    return (((value & (1 << (MASKSIZE - 1))) > 0) << BitPositionLeft) | Mask<Type, BitPositionsRight...>::distribute(value);
+  }
 };
 
 template<typename Type, size_t BitPosition>
 struct Mask<Type, BitPosition>
 {
   static constexpr Type BITMASK{1 << BitPosition};
-  static constexpr size_t MASKSIZE{1};
 
   template<Type Value>
   static Type distribute(void) { return ((Value & 1) > 0) << BitPosition; }
+  static Type distribute(const Type value) { return ((value & 1) > 0) << BitPosition; }
 
   static Type readFrom(const Type src) { return (src & (1 << BitPosition)) > 0; }
 };
--- src/test.cpp
+++ src/test.cpp
@@ -5,13 +5,18 @@
 
 typedef smartmask::Mask<uint32_t, 31, 20, 0> TestMask;
 
+static constexpr smartmask::Mask<uint32_t, 31, 20, 0> MyMask;
+
 int main()
 {
-  volatile uint32_t reg{0xff00ff00};
+  volatile uint32_t reg{0xff00ff00u};
   std::cout << "reg before:\n" << std::bitset<32>{reg} << "\n";
-  std::cout << "value before:\n" << TestMask::readFrom(reg) << "\n";
-  TestMask::writeTo<0b011>(reg);
-  std::cout << "reg after:\n" << std::bitset<32>{reg} << "\n";
-  std::cout << "value after:\n" << TestMask::readFrom(reg) << "\n";
+  std::cout << "value before:\n" << MyMask.readFrom(reg) << "\n";
+  MyMask.writeTo<0b011u>(reg);
+  std::cout << "reg after template write:\n" << std::bitset<32>{reg} << "\n";
+  std::cout << "value after template write:\n" << MyMask.readFrom(reg) << "\n";
+  MyMask.writeTo(0b111u, reg);
+  std::cout << "reg after non-template write:\n" << std::bitset<32>{reg} << "\n";
+  std::cout << "value after non-template write:\n" << MyMask.readFrom(reg) << "\n";
   return 0;
 }
