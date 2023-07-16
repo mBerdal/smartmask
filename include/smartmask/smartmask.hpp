f84617c3d77b5fd4dffb12c13533052ec41a93dd
Magnus Berdal <magnus.berdal@gmail.com>
2023-07-16 23:03:38 +0200

    Fix narrowing conversion

--- include/smartmask/smartmask.hpp
+++ include/smartmask/smartmask.hpp
@@ -8,20 +8,21 @@ namespace smartmask
 template<typename Type, size_t BitPositionLeft, size_t... BitPositionsRight>
 struct Mask
 {
-  static constexpr Type BITMASK{(1 << BitPositionLeft) | Mask<Type, BitPositionsRight...>::BITMASK};
-  static constexpr size_t MASKSIZE{1 + sizeof...(BitPositionsRight)};
+  static constexpr Type ONE{static_cast<Type>(1)};
+  static constexpr Type BITMASK{(ONE << BitPositionLeft) | Mask<Type, BitPositionsRight...>::BITMASK};
+  static constexpr size_t MASKSIZE{ONE + sizeof...(BitPositionsRight)};
 
   template<Type Value>
   static void writeTo(volatile Type& dest)
   {
-    static_assert(Value < (1 << MASKSIZE));
+    static_assert(Value < (ONE << MASKSIZE));
     dest = (dest & ~BITMASK) | distribute<Value>();
   }
 
   template<Type Value>
   static void writeTo(Type& dest)
   {
-    static_assert(Value < (1 << MASKSIZE));
+    static_assert(Value < (ONE << MASKSIZE));
     dest = (dest & ~BITMASK) | distribute<Value>();
   }
 
@@ -36,37 +37,38 @@ struct Mask
 
   static Type readFrom(const Type src)
   {
-    return (((src & (1 << BitPositionLeft)) > 0) << (MASKSIZE - 1)) | Mask<Type, BitPositionsRight...>::readFrom(src);
+    return (((src & (ONE << BitPositionLeft)) > 0) << (MASKSIZE - ONE)) | Mask<Type, BitPositionsRight...>::readFrom(src);
   }
 
   template<Type Value>  
   static Type distribute(void)
   {
-    return (((Value & (1 << (MASKSIZE - 1))) > 0) << BitPositionLeft) | Mask<Type, BitPositionsRight...>::template distribute<Value>();
+    return (((Value & (ONE << (MASKSIZE - ONE))) > 0) << BitPositionLeft) | Mask<Type, BitPositionsRight...>::template distribute<Value>();
   }
   static Type distribute(const Type value)
   {
-    return (((value & (1 << (MASKSIZE - 1))) > 0) << BitPositionLeft) | Mask<Type, BitPositionsRight...>::distribute(value);
+    return (((value & (ONE << (MASKSIZE - ONE))) > 0) << BitPositionLeft) | Mask<Type, BitPositionsRight...>::distribute(value);
   }
 };
 
 template<typename Type, size_t BitPosition>
 struct Mask<Type, BitPosition>
 {
-  static constexpr Type BITMASK{1 << BitPosition};
-  static constexpr Type MASKSIZE{1};
+  static constexpr Type ONE{static_cast<Type>(1)};
+  static constexpr Type BITMASK{ONE << BitPosition};
+  static constexpr Type MASKSIZE{ONE};
 
   template<Type Value>
   static void writeTo(volatile Type& dest)
   {
-    static_assert(Value < (1 << MASKSIZE));
+    static_assert(Value < (ONE << MASKSIZE));
     dest = (dest & ~BITMASK) | distribute<Value>();
   }
 
   template<Type Value>
   static void writeTo(Type& dest)
   {
-    static_assert(Value < (1 << MASKSIZE));
+    static_assert(Value < (ONE << MASKSIZE));
     dest = (dest & ~BITMASK) | distribute<Value>();
   }
 
@@ -79,11 +81,11 @@ struct Mask<Type, BitPosition>
     dest = (dest & ~BITMASK) | distribute(value);
   }
 
-  static Type readFrom(const Type src) { return (src & (1 << BitPosition)) > 0; }
+  static Type readFrom(const Type src) { return (src & (ONE << BitPosition)) > 0; }
   
   template<Type Value>
-  static Type distribute(void) { return ((Value & 1) > 0) << BitPosition; }
-  static Type distribute(const Type value) { return ((value & 1) > 0) << BitPosition; }
+  static Type distribute(void) { return ((Value & ONE) > 0) << BitPosition; }
+  static Type distribute(const Type value) { return ((value & ONE) > 0) << BitPosition; }
 };
   
 } // namespace smartmask
